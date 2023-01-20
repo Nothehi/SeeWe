17b474abf5a9129063cf958aa8bb46972f6c1c74
nothehi <nodehi7813@gmail.com>
2023-01-20 13:14:18 +0330

    Fix hydration

--- pages/index.vue
+++ pages/index.vue
@@ -15,134 +15,213 @@
         </section>
 
         <!-- Skill Summary -->
-        <TheSegment title="Skill Summary">
-            <ul>
-                <li>🔹 PHP debugging and error tracking</li>
-                <li>🔹 JavaScript debugging and error tracking</li>
-                <li>🔹 Laravel debugging and error tracking</li>
-                <li>🔹 Laravel package development</li>
-                <li>🔹 Database analysis & design</li>
-            </ul>
-        </TheSegment>
+        <section class="flex md:flex-row flex-col">
+            <aside class="md:w-1/5 md:border-r pt-5">
+                <h3 class="font-bold font-montserrat">Skill Summary</h3>
+            </aside>
+            <article class="md:w-4/5 font-spacemono px-2 md:pt-5">
+                <ul>
+                    <li>🔹 PHP debugging and error tracking</li>
+                    <li>🔹 JavaScript debugging and error tracking</li>
+                    <li>🔹 Laravel debugging and error tracking</li>
+                    <li>🔹 Laravel package development</li>
+                    <li>🔹 Database analysis & design</li>
+                </ul>
+            </article>
+        </section>
 
         <!-- Jobs -->
-        <TheSegment title="Jobs">
-            <TheJob v-for="job in jobs" v-bind="job" />
-        </TheSegment>
+        <section class="flex md:flex-row flex-col">
+            <aside class="md:w-1/5 md:border-r pt-10">
+                <h3 class="font-bold font-montserrat">Jobs</h3>
+            </aside>
+            <article class="md:w-4/5 font-spacemono px-2 md:pt-10">
+                <ul>
+                    <li class="flex flex-col mb-5">
+                        <p class="text-lg font-medium underline">Backend Developer (Contract)</p>
+                        <span class="text-sm">April 2022 – now</span>
+                        <p>At <a href="https://www.partsoftware.com/" class="font-medium text-blue-900">Part
+                                Software Group</a></p>
+                        <span class="text-xs">🛠️ #Laravel, #Vuejs, #PostgreSQL, #TDD</span>
+                    </li>
+                    <li class="flex flex-col mb-5">
+                        <p class="text-lg font-medium underline">Fullstack Developer (Contract)</p>
+                        <span class="text-sm">February 2020 – January 2021</span>
+                        <p>At <a href="https://www.hamtapay.net/" class="font-medium text-blue-900">Hamta
+                                Payment</a></p>
+                        <span class="text-xs">🛠️ #Laravel, #Lumen, #Nodejs, #Vuejs, #MySQL, #Redis,
+                            #Microservice</span>
+                    </li>
+                    <li class="flex flex-col mb-5">
+                        <p class="text-lg font-medium underline">Backend Developer (Contract)</p>
+                        <span class="text-sm">March 2019 – September 2019</span>
+                        <p>At <a href="http://32negin.com/" class="font-medium text-blue-900">32 Negin</a></p>
+                        <span class="text-xs">🛠️ #Laravel, #MySQL, #Microservice</span>
+                    </li>
+                    <li class="flex flex-col mb-5">
+                        <p class="text-lg font-medium underline">Web Developer (Part-Time)</p>
+                        <span class="text-sm">December 2018 – March 2019</span>
+                        <p>At <a href="#" class="font-medium text-blue-900">Propagation Office</a></p>
+                        <span class="text-xs">🛠️ #PHP, #Laravel, #Wordpress, #MySQL</span>
+                    </li>
+                    <li class="flex flex-col mb-5">
+                        <p class="text-lg font-medium underline">PHP Developer (Part-Time)</p>
+                        <span class="text-sm">September 2018 – December 2018</span>
+                        <p>At <a href="#" class="font-medium text-blue-900">Smart Business</a></p>
+                        <span class="text-xs">🛠️ #PHP, #Wordpress, #MySQL</span>
+                    </li>
+                    <li class="flex flex-col mb-5">
+                        <p class="text-lg font-medium underline">PHP Developer (Part-Time)</p>
+                        <span class="text-sm">June 2018 – October 2018</span>
+                        <p>At <a href="https://faraket.com/" class="font-medium text-blue-900">Faraket</a></p>
+                        <span class="text-xs">🛠️ #PHP, #PrestaShop</span>
+                    </li>
+                </ul>
+            </article>
+        </section>
 
         <!-- Main Technical Summary -->
-        <TheSegment title="Main Technical Summary">
-            <ul>
-                <li v-for="skill in skills">🔸 {{ skill.join(', ') }}</li>
-            </ul>
-        </TheSegment>
+        <section class="flex md:flex-row flex-col">
+            <aside class="md:w-1/5 md:border-r pt-10">
+                <h3 class="font-bold font-montserrat">Main Technical Summary</h3>
+            </aside>
+
+            <article class="md:w-4/5 font-spacemono px-2 md:pt-10">
+                <ul>
+                    <li>🔸 HTML, CSS, JavaScript, PHP</li>
+                    <li>🔸 MySQL, MariaDB, phpMyAdmin, PostgreSQL</li>
+                    <li>🔸 Git, GitLab, GitHub</li>
+                    <li>🔸 OOP, SOLID, Design Pattern, MVC, Clean Code, TDD</li>
+                    <li>🔸 Laravel, Lumen, Composer</li>
+                    <li>🔸 Vuejs, Tailwind, Npm, Nuxt</li>
+                    <!-- <li>🔸 Python, Django, Flask</li> -->
+                    <li>🔸 Agile, Scrum</li>
+                </ul>
+            </article>
+        </section>
 
         <!-- Academic Degrees -->
-        <TheSegment title="Academic Degrees">
-            <ul>
-                <li class="flex flex-col mb-5" v-for="degree in degrees">
-                    <p class="font-medium">{{ degree.finished ? '🎓' : '' }} {{ degree.grade }} Degree in {{
-                            degree.field
-                    }}</p>
-                    <span class="text-sm">📅 {{ degree.from }} – {{ degree.to }}</span>
-                    <span>🏫 {{ degree.university }}</span>
-                </li>
-            </ul>
-        </TheSegment>
+        <section class="flex md:flex-row flex-col">
+            <aside class="md:w-1/5 md:border-r pt-10">
+                <h3 class="font-bold font-montserrat">Academic Degrees</h3>
+            </aside>
+
+            <article class="md:w-4/5 font-spacemono px-2 md:pt-10">
+                <ul>
+                    <li class="flex flex-col mb-5">
+                        <p class="font-medium">Bachelor Degree in Software Engineering</p>
+                        <span class="text-sm">📅 September 2020 – now</span>
+                        <span>🏫 Montazeri Technical College of Mashhad</span>
+                    </li>
+                    <li class="flex flex-col mb-5">
+                        <p class="font-medium">🎓 Associate Degree in Software Engineering</p>
+                        <span class="text-sm">📅 September 2017 – June 2019</span>
+                        <span>🏫 Montazeri Technical College of Mashhad</span>
+                    </li>
+                </ul>
+            </article>
+        </section>
 
         <!-- Personality -->
-        <TheSegment title="Personality">
-            <ul>
-                <li>- Team working</li>
-                <li>- Flexibility</li>
-                <li>- Creative</li>
-                <li>- Problem Solving</li>
-                <li>- Geek and interested in learning new things</li>
-            </ul>
-        </TheSegment>
+        <section class="flex md:flex-row flex-col">
+            <aside class="md:w-1/5 md:border-r pt-5">
+                <h3 class="font-bold font-montserrat">Personality</h3>
+            </aside>
+            <article class="md:w-4/5 font-spacemono px-2 md:pt-5">
+                <ul>
+                    <li>- Team working</li>
+                    <li>- Flexibility</li>
+                    <li>- Creative</li>
+                    <li>- Problem Solving</li>
+                    <li>- Geek and interested in learning new things</li>
+                </ul>
+            </article>
+        </section>
 
         <footer class="pt-2 border-t">
-            <p class="text-center"><span class="transform rotate-180 inline-block">&copy;</span> Copyleft 2022
+            <p class="text-center"><span class="transform rotate-180 inline-block">&copy;</span> Copyleft 2023
             </p>
         </footer>
     </main>
 </template>
 
+<!--
 <script setup>
 const jobs = [
-    {
-        title: "Backend Developer (Contract)",
-        from: "April 2022",
-        company: "Part Software Group",
-        link: "https://www.partsoftware.com/",
-        skills: ['Laravel', 'Vuejs', 'PostgreSQL', 'TDD'],
-    },
-    {
-        title: "Backend Developer (Contract)",
-        from: "February 2020",
-        to: "January 2021",
-        company: "Hamta Payment",
-        link: "https://www.hamtapay.net/",
-        skills: ['Laravel', 'Lumen', 'Nodejs', 'Vuejs', 'MySQL', 'Redis', 'Microservice'],
-    },
-    {
-        title: "Backend Developer (Contract)",
-        from: "March 2019",
-        to: "September 2019",
-        company: "32 Negin",
-        link: "http://32negin.com/",
-        skills: ['Laravel', 'MySQL', 'Microservice'],
-    },
-    {
-        title: "Web Developer (PartTime)",
-        from: "December 2018",
-        to: "March 2019",
-        company: "Propagation Office",
-        skills: ['PHP', 'Laravel', 'MySQL', 'Wordpress'],
-    },
-    {
-        title: "PHP Developer (PartTime)",
-        from: "September 2018",
-        to: "December 2018",
-        company: "Smart Business",
-        link: "https://faraket.com/",
-        skills: ['PHP', 'PrestaShop', 'MySQL'],
-    },
-    {
-        title: "PHP Developer (PartTime)",
-        from: "June 2018",
-        to: "October 2018",
-        company: "Faraket",
-        skills: ['PHP', 'Wordpress', 'MySQL'],
-    }
+  {
+    title: 'Backend Developer (Contract)',
+    from: 'April 2022',
+    company: 'Part Software Group',
+    link: 'https://www.partsoftware.com/',
+    skills: ['Laravel', 'Vuejs', 'PostgreSQL', 'TDD']
+  },
+  {
+    title: 'Backend Developer (Contract)',
+    from: 'February 2020',
+    to: 'January 2021',
+    company: 'Hamta Payment',
+    link: 'https://www.hamtapay.net/',
+    skills: ['Laravel', 'Lumen', 'Nodejs', 'Vuejs', 'MySQL', 'Redis', 'Microservice']
+  },
+  {
+    title: 'Backend Developer (Contract)',
+    from: 'March 2019',
+    to: 'September 2019',
+    company: '32 Negin',
+    link: 'http://32negin.com/',
+    skills: ['Laravel', 'MySQL', 'Microservice']
+  },
+  {
+    title: 'Web Developer (PartTime)',
+    from: 'December 2018',
+    to: 'March 2019',
+    company: 'Propagation Office',
+    skills: ['PHP', 'Laravel', 'MySQL', 'Wordpress']
+  },
+  {
+    title: 'PHP Developer (PartTime)',
+    from: 'September 2018',
+    to: 'December 2018',
+    company: 'Smart Business',
+    link: 'https://faraket.com/',
+    skills: ['PHP', 'PrestaShop', 'MySQL']
+  },
+  {
+    title: 'PHP Developer (PartTime)',
+    from: 'June 2018',
+    to: 'October 2018',
+    company: 'Faraket',
+    skills: ['PHP', 'Wordpress', 'MySQL']
+  }
 ]
 
 const skills = [
-    ['HTML', 'CSS', 'JavaScript', 'PHP'],
-    ['MySQL', 'MariaDB', 'phpMyAdmin', 'PostgreSQL'],
-    ['Git', 'GitLab', 'GitHub'],
-    ['OOP', 'SOLID', 'Design Pattern', 'MVC', 'Clean Code', 'TDD'],
-    ['Laravel', 'Lumen', 'Composer'],
-    ['Vuejs', 'Tailwind', 'Npm', 'Nuxt'],
-    ['Agile', 'Scrum'],
+  ['HTML', 'CSS', 'JavaScript', 'PHP'],
+  ['MySQL', 'MariaDB', 'phpMyAdmin', 'PostgreSQL'],
+  ['Git', 'GitLab', 'GitHub'],
+  ['OOP', 'SOLID', 'Design Pattern', 'MVC', 'Clean Code', 'TDD'],
+  ['Laravel', 'Lumen', 'Composer'],
+  ['Vuejs', 'Tailwind', 'Npm', 'Nuxt'],
+  ['Agile', 'Scrum']
 ]
 
 const degrees = [
-    {
-        grade: "Bachelor",
-        field: "Software Engineering",
-        from: "September 2020",
-        to: "now",
-        university: "Montazeri Technical College of Mashhad",
-        finished: false,
-    },
-    {
-        grade: "Associate",
-        field: "Software Engineering",
-        from: "September 2017",
-        to: "June 2019",
-        university: "Montazeri Technical College of Mashhad",
-        finished: true,
-    },
+  {
+    grade: 'Bachelor',
+    field: 'Software Engineering',
+    from: 'September 2020',
+    to: 'now',
+    university: 'Montazeri Technical College of Mashhad',
+    finished: false
+  },
+  {
+    grade: 'Associate',
+    field: 'Software Engineering',
+    from: 'September 2017',
+    to: 'June 2019',
+    university: 'Montazeri Technical College of Mashhad',
+    finished: true
+  }
 ]
 </script>
+-->
